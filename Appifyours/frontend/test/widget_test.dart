6c395cec51ed469cc512f2c3e5d75046818de5b1
JBBSoftech <jbbsoftech@gmail.com>
2025-12-21 10:30:31 +0530

    Update Appifyours/frontend/test/widget_test.dart

--- Appifyours/frontend/test/widget_test.dart
+++ Appifyours/frontend/test/widget_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:appifyours/main.dart';
+  import 'package:flutter_test/flutter_test.dart';
+  import 'package:appifyours/main.dart';
 
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  void main() {
+    testWidgets('App smoke test', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
 
-    // Verify that the app builds without errors
-    expect(find.byType(MaterialApp), findsOneWidget);
-  });
-}
+      // Verify that the app builds without errors
+      expect(find.byType(MaterialApp), findsOneWidget);
+    });
+  }

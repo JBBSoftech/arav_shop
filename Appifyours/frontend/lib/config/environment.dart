d0cbc4fe0e8a52a8c9f133960ff29035f4aad46f
JBBSoftech <jbbsoftech@gmail.com>
2025-12-23 10:19:58 +0530

    Update Appifyours/frontend/lib/config/environment.dart

--- Appifyours/frontend/lib/config/environment.dart
+++ Appifyours/frontend/lib/config/environment.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Environment {
   // Get API base URL from .env file
   static String get apiBase {
-    return dotenv.env['API_BASE'] ?? 'http://192.168.0.17:5000';
+    return dotenv.env['API_BASE'] ?? 'http://192.168.0.3:5000';
   }
   
   // Add other environment variables here as needed
